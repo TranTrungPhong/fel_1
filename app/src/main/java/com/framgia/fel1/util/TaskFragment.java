168348cb6293471961f1ecf9d1b1d3ddaf68e8f9
vu.duy.chuong <vu.duy.chuong@framgia.com>
2016-05-17 11:50:57 +0700

    fix_show_result

--- app/src/main/java/com/framgia/fel1/util/TaskFragment.java
+++ app/src/main/java/com/framgia/fel1/util/TaskFragment.java
@@ -13,20 +13,12 @@ public class TaskFragment extends Fragment {
     private TaskCallbacks mCallbacks;
     private DummyTask mTask;
 
-    /**
-     * Hold a reference to the parent Activity so we can report the task's current progress and
-     * results. The Android framework will pass us a reference to the newly created Activity after
-     * each configuration change.
-     */
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         mCallbacks = (TaskCallbacks) activity;
     }
 
-    /**
-     * This method will only be called once when the retained Fragment is first created.
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,27 +27,17 @@ public class TaskFragment extends Fragment {
         setRetainInstance(true);
     }
 
-    /**
-     * Set the callback to null so we don't accidentally leak the Activity instance.
-     */
     @Override
     public void onDetach() {
         super.onDetach();
         mCallbacks = null;
     }
 
-    public void startInBackground(String[] param){
+    public void startInBackground(String[] param) {
         mTask = new DummyTask();
         mTask.execute(param);
     }
 
-    /**
-     * A dummy task that performs some (dumb) background work and proxies progress updates and
-     * results back to the Activity.
-     * <p>
-     * Note that we need to check if the callbacks are null in each method in case they are invoked
-     * after the Activity's and Fragment's onDestroy() method have been called.
-     */
     private class DummyTask extends AsyncTask<String, String, String> {
 
         @Override
@@ -92,10 +74,6 @@ public class TaskFragment extends Fragment {
         }
     }
 
-    /**
-     * Callback interface through which the fragment will report the task's progress and results
-     * back to the Activity.
-     */
     public interface TaskCallbacks {
         void onPreExecute();
 
